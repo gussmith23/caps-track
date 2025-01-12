b34e7fdb179ee5ddd7633477ba3fea526cd3fd5a
Gus Smith <guscomps@gmail.com>
2025-01-12 21:23:28 +0700

    More work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post, Render, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @Inject('SHEET_PROVIDER') private sheet) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  async getIndex() {
+    return Promise.all([this.sheet.activeAndConcludedGames(), this.sheet.getAllPlayers()]).then(([[activeGames, concludedGames], players]) => {
+      return { activeGames: activeGames, concludedGames: concludedGames, players: players };
+    });
+  }
+
+  @Post('newGame')
+  async postNewGame(@Res() res, @Body() body) {
+    let id = await this.sheet.newGame(body.player1, body.player2, body.player3, body.player4);
+    res.redirect(`/game/${id}`);
+  }
+
+  @Get('game/:id')
+  @Render('game')
+  async getGame(@Param() params) {
+    let game = await this.sheet.getGame(params.id);
+    let [player1, player2, player3, player4] = await this.sheet.getPlayers([game.player1, game.player2, game.player3, game.player4]);
+
+    return { game: game, player1: player1, player2: player2, player3: player3, player4: player4 };
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { sheetProvider } from './sheet.provider';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, sheetProvider],
 })
-export class AppModule {}
+export class AppModule { }
