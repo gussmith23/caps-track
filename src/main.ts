f638dc8415c966e5626eaf8f1aec805ea4d8e430
Gus Smith <guscomps@gmail.com>
2025-01-20 20:35:18 +0700

    Use base template

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { create, engine } from 'express-handlebars';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.engine('.hbs', engine({ extname: '.hbs', helpers: { formatPlayer }, layoutsDir: join(__dirname, '..', 'views'), defaultLayout: '' }));
+  app.engine('.hbs', engine({ extname: '.hbs', helpers: { formatPlayer }, defaultLayout: 'default' }));
   app.setViewEngine('hbs');
   await app.listen(process.env.PORT ?? 3000);
 }
