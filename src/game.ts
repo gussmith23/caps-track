af82eb00431806edde69c483f25a7c6b9c6d5cc0
Gus Smith <guscomps@gmail.com>
2025-01-21 19:59:09 +0700

    Do some validation

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,15 @@ export class Game {
   constructor(public id: number, public player1id: number, public player2id: number, public player3id: number, public player4id: number, public beganAt: Date, public endedAt?: Date) { };
 
   static fromRow(row: GoogleSpreadsheetRow) {
-    return new Game(row.get('id'), row.get('player1'), row.get('player2'), row.get('player3'), row.get('player4'), new Date(row.get('beganAt')), row.get('endedAt') ? new Date(row.get('endedAt')) : undefined);
+    // TODO(@gussmith23): It feels like there should be a more TypeScript-y way
+    // of doing this. Apparently get() can return undefined and there's no way
+    // to make it fail if it does.
+    let errorIfUndefined = (field: string) => {
+      if (typeof row.get(field) === 'undefined') {
+        throw new Error(`Game row is missing ${field}`);
+      }
+      return row.get(field);
+    };
+    return new Game(errorIfUndefined('id'), errorIfUndefined('player1'), errorIfUndefined('player2'), errorIfUndefined('player3'), errorIfUndefined('player4'), new Date(errorIfUndefined('beganAt')), row.get('endedAt') ? new Date(row.get('endedAt')) : undefined);
   }
 };
--- src/player.ts
+++ src/player.ts
@@ -4,6 +4,15 @@ export class Player {
   constructor(public id: number, public name: string) { };
 
   static fromRow(row: GoogleSpreadsheetRow) {
-    return new Player(row.get('id'), row.get('name'));
+    // TODO(@gussmith23): It feels like there should be a more TypeScript-y way
+    // of doing this. Apparently get() can return undefined and there's no way
+    // to make it fail if it does.
+    let errorIfUndefined = (field: string) => {
+      if (typeof row.get(field) === 'undefined') {
+        throw new Error(`Player row is missing ${field}`);
+      }
+      return row.get(field);
+    };
+    return new Player(errorIfUndefined('id'), errorIfUndefined('name'));
   }
 };
