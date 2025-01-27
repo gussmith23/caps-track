1164c3b439ec72078477f648e64fdc1580a1ed84
Gus Smith <guscomps@gmail.com>
2025-01-27 21:47:46 +0700

    Change name of field

--- hbs_helpers/helpers.ts
+++ hbs_helpers/helpers.ts
@@ -1,5 +1,5 @@
 import { Player } from "src/player"
 
 export function formatPlayer(player: Player) {
-  return `<span style="color: ${player.color}">${player.name}</span>`;
+  return `<span style="color: ${player.nameColor}">${player.name}</span>`;
 }
