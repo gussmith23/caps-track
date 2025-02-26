e8fed5b6ce8c830e91f7573534b5d44fed3829d5
Gus Smith <guscomps@gmail.com>
2025-02-26 12:48:27 +0700

    Bunch of work

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { sheetProvider } from './sheet.provider';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [],
+  imports: [EventEmitterModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, sheetProvider],
 })
